3549647ee42266c2b4ced15b34db0ba78c9569fa
weequan <wee@hyperlegend.onmicrosoft.com>
2022-08-22 13:36:28 +0400

    update

--- remote_update/updatescript.js
+++ remote_update/updatescript.js
@@ -4,57 +4,73 @@
 var cp = require('child_process')
 
 var BASE = '/tmp/extract'
-var PACKAGE = Base + "/package"
+var PACKAGE = BASE + "/package"
 var INSTALLED = "/opt"
 
 const NeeedNpmInstall = false
 
-function frontend(){
-  return new Promise((resolve)=>{
-    cp.exec(`rm ${INSTALLED}/hyper-browser/build.zip`, null, function(){
+function frontend() {
+  return new Promise((resolve) => {
+    const cmd = `rm ${INSTALLED}/hyper-browser/build.zip`
+    console.log(cmd)
+    cp.exec(cmd, null, function () {
       return resolve()
     })
-  }).then(()=>{
+  }).then(() => {
     return new Promise((resolve) => {
-      cp.exec(`rm -rf ${INSTALLED}/hyper-browser/build`, null, function () {
+      const cmd = `rm -rf ${INSTALLED}/hyper-browser/build`
+      console.log(cmd)
+      cp.exec(cmd, null, function () {
         return resolve()
       })
     })
   }).then(() => {
     return new Promise((resolve) => {
-      cp.exec(`cp ${PACKAGE}/build.zip ${INSTALLED}/hyper-browser`, null, function () {
+      const cmd = `cp ${PACKAGE}/build.zip ${INSTALLED}/hyper-browser`
+      console.log(cmd)
+      cp.exec(cmd, null, function () {
         return resolve()
       })
     })
   }).then(() => {
     return new Promise((resolve) => {
-      cp.exec(`unzip ${INSTALLED}/hyper-browser/build.zip`, null, function () {
+      const cmd = `unzip -o ${INSTALLED}/hyper-browser/build.zip -d ${INSTALLED}/hyper-browser`
+      console.log(cmd)
+      cp.exec(cmd, null, function () {
         return resolve()
       })
     })
-  }).then(()=>{
+  }).then(() => {
     return new Promise((resolve) => {
-      cp.exec('supervisorctl restart hyper-browser-server hyper-browser', null, function () {
+      const cmd = 'supervisorctl restart hyper-server-browser hyper-browser'
+      console.log(cmd)
+      cp.exec(cmd, null, function () {
         return resolve()
       })
     })
   })
 }
 
-function hyperMachine(){
-  return new Promise((resolve, reject)=>{
-    cp.exec(`cp -fR ${PACKAGE}/hyper-machine/* ${INSTALLED}/hyper-machine`, null, function () {
+function hyperMachine() {
+  return new Promise((resolve, reject) => {
+    const cmd = `cp -fR ${PACKAGE}/hyper-machine/* ${INSTALLED}/hyper-machine`
+    console.log(cmd)
+    cp.exec(cmd, null, function () {
       return resolve()
     })
   })
 }
 
-function start(){
-  frontend().then(()=>{
+function start() {
+  frontend().then(() => {
     return hyperMachine();
-  }).then(()=>{
-    cp.exec('supervisorctl restart hyper-janitor hyper-server hyper-updater hyper-watchdog', null, function () {
-      return resolve()
+  }).then(() => {
+    return new Promise((resolve, reject) => {
+      const cmd = 'supervisorctl restart hyper-janitor hyper-server'
+      console.log(cmd)
+      cp.exec(cmd, null, function () {
+        return resolve()
+      })
     })
   })
 }
